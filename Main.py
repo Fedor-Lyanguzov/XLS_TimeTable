ae835ad4a0b354ad829d432ddbd0e90d1b80aaab
danyarmarkin <danila.iarmarkin@gmail.com>
2021-09-04 22:42:29 +0300

    add table format

--- Main.py
+++ Main.py
@@ -4,6 +4,46 @@ from TimeTable import *
 import xlsxwriter
 
 
+def box(workbook, worksheet, first_row, first_col, rows_count, cols_count):
+
+    # top left corner
+    worksheet.conditional_format(first_row, first_col,
+                                 first_row, first_col,
+                                 {'type': 'formula', 'criteria': 'True',
+                                  'format': workbook.add_format({'top': 2, 'left': 2})})
+    # top right corner
+    worksheet.conditional_format(first_row, first_col + cols_count - 1,
+                                 first_row, first_col + cols_count - 1,
+                                 {'type': 'formula', 'criteria': 'True',
+                                  'format': workbook.add_format({'top': 2, 'right': 2})})
+    # bottom left corner
+    worksheet.conditional_format(first_row + rows_count - 1, first_col,
+                                 first_row + rows_count - 1, first_col,
+                                 {'type': 'formula', 'criteria': 'True',
+                                  'format': workbook.add_format({'bottom': 2, 'left': 2})})
+    # bottom right corner
+    worksheet.conditional_format(first_row + rows_count - 1, first_col + cols_count - 1,
+                                 first_row + rows_count - 1, first_col + cols_count - 1,
+                                 {'type': 'formula', 'criteria': 'True',
+                                  'format': workbook.add_format({'bottom': 2, 'right': 2})})
+
+    # top
+    worksheet.conditional_format(first_row, first_col + 1,
+                                 first_row, first_col + cols_count - 2,
+                                 {'type': 'formula', 'criteria': 'True', 'format': workbook.add_format({'top': 2})})
+    # left
+    worksheet.conditional_format(first_row + 1,              first_col,
+                                 first_row + rows_count - 2, first_col,
+                                 {'type': 'formula', 'criteria': 'True', 'format': workbook.add_format({'left': 2})})
+    # bottom
+    worksheet.conditional_format(first_row + rows_count - 1, first_col + 1,
+                                 first_row + rows_count - 1, first_col + cols_count - 2,
+                                 {'type': 'formula', 'criteria': 'True', 'format': workbook.add_format({'bottom': 2})})
+    # right
+    worksheet.conditional_format(first_row + 1,              first_col + cols_count - 1,
+                                 first_row + rows_count - 2, first_col + cols_count - 1,
+                                 {'type': 'formula', 'criteria': 'True', 'format': workbook.add_format({'right': 2})})
+
 #  Import timetable
 workbook = load_workbook('cl_sum.xlsx')
 first_sheet = workbook.get_sheet_names()[0]
@@ -21,13 +61,16 @@ for student_class in range(5, 320, 10):
             lesson = []
             for param in range(10):
                 cell = worksheet.cell(row=student_class + param, column=2 + day * 8 + lesson_number).value
-                if cell != None:
+                if cell is not None:
                     lesson.append(cell)
             if len(lesson) == 4:
-                items.append(Item(student_class=name, teacher=lesson[1], subject=lesson[0], group=1, day=day, auditory=lesson[3], lesson_number=lesson_number))
+                items.append(Item(student_class=name, teacher=lesson[1], subject=lesson[0], group=1, day=day,
+                                  auditory=lesson[3], lesson_number=lesson_number))
             elif len(lesson) == 10:
-                items.append(Item(student_class=name, teacher=lesson[1], subject=lesson[0], group=1, day=day, auditory=lesson[3], lesson_number=lesson_number))
-                items.append(Item(student_class=name, teacher=lesson[6], subject=lesson[5], group=2, day=day, auditory=lesson[8], lesson_number=lesson_number))
+                items.append(Item(student_class=name, teacher=lesson[1], subject=lesson[0], group=1, day=day,
+                                  auditory=lesson[3], lesson_number=lesson_number))
+                items.append(Item(student_class=name, teacher=lesson[6], subject=lesson[5], group=2, day=day,
+                                  auditory=lesson[8], lesson_number=lesson_number))
 
 timetable = TimeTable()
 
@@ -64,24 +107,33 @@ start_y = 2
 lessons = 8
 width = 4
 
-for y in range(start_y - 1, start_y + 7 * lessons):
-    for x in range(start_x - 3, start_x + (len(timetable.classes) + 1) * width):
-        students_worksheet.write(y, x, "", cell_format)
+students_worksheet.set_row(start_y - 1, start_y + 7 * lessons, cell_format)
+
+# for y in range(start_y - 1, start_y + 7 * lessons):
+#     for x in range(start_x - 3, start_x + (len(timetable.classes) + 1) * width):
+#         students_worksheet.write(y, x, "", cell_format)
 
 ind = 0
 for st_class in timetable.classes:
     for day in range(6):
+        box(workbook, students_worksheet, start_y + day * lessons, start_x + ind * width, lessons, width)
         for lesson in timetable.classes_timetable[st_class][day]:
-            students_worksheet.write(start_y + day * lessons + lesson.number, start_x + ind * width + lesson.group * 2, lesson.name, cell_format)
-            students_worksheet.write(start_y + day * lessons + lesson.number, start_x + ind * width + lesson.group * 2 + 1, lesson.auditory, cell_format)
+            students_worksheet.write(start_y + day * lessons + lesson.number, start_x + ind * width + lesson.group * 2,
+                                     lesson.name)
+            students_worksheet.write(start_y + day * lessons + lesson.number,
+                                     start_x + ind * width + lesson.group * 2 + 1, lesson.auditory)
         # Add classes names in header
-        students_worksheet.merge_range(start_y - 1, start_x + ind * width, start_y - 1, start_x + ind * width + 3, st_class, merge_format)
+        students_worksheet.merge_range(start_y - 1, start_x + ind * width, start_y - 1, start_x + ind * width + 3,
+                                       st_class, merge_format)
     ind += 1
 
 for day in range(0, 6):
     for lesson in range(lessons):
-        students_worksheet.write(start_y + day * lessons + lesson, start_x - 2, str(lesson), cell_format)
-    students_worksheet.merge_range(start_y + day * lessons, start_x - 3, start_y + day * lessons + lessons - 1, start_x - 3, getDay(day), merge_format)
+        box(workbook, students_worksheet, start_y + day * lessons, start_x - 2, lessons, 2)
+        students_worksheet.write(start_y + day * lessons + lesson, start_x - 2, str(lesson))
+        students_worksheet.write(start_y + day * lessons + lesson, start_x - 1, getLessonsTime(lesson))
+    students_worksheet.merge_range(start_y + day * lessons, start_x - 3, start_y + day * lessons + lessons - 1,
+                                   start_x - 3, getDay(day), merge_format)
 
 print("done!")
 workbook.close()
--- TimeTable.py
+++ TimeTable.py
@@ -27,3 +27,21 @@ def getDay(day):
         return "Пятн."
     if day == 5:
         return "Субб."
+
+def getLessonsTime(lesson):
+    if lesson == 0:
+        return "08:20 - 09:05"
+    if lesson == 1:
+        return "09:15 - 10:00"
+    if lesson == 2:
+        return "10:10 - 10:55"
+    if lesson == 3:
+        return "11:10 - 11:55"
+    if lesson == 4:
+        return "12:10 - 12:55"
+    if lesson == 5:
+        return "13:25 - 14:10"
+    if lesson == 6:
+        return "14:20 - 15:05"
+    if lesson == 7:
+        return "15:15 - 16:00"
